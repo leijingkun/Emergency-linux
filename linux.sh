852c33a8248be33343ebfac87ec757f539f16de8
leijingkun <2092507229@qq.com>
2024-04-19 01:20:18 +0800

    First Commit

---
+++ linux.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+function Basic_Info(){
+echo "===============基本信息============"
+    echo "主机名:$(hostname)"
+    echo "当前用户:$(whoami)"
+
+echo "=================end=============="
+}
+
+
+Command_Exist(){
+    if command -v $1 >/dev/null 2>&1; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+#入侵排查
+Invade_Identify(){
+    echo "================入侵检测==============="
+    echo  "特权用户:"
+    awk -F: '$3==0{print $1}' /etc/passwd
+    echo "可登录用户"
+    awk -F: '$NF!="/usr/sbin/nologin"{print $1}' /etc/passwd |paste -sd ','
+    echo "可以远程登录的账号信息"
+    awk '/\$1|\$6/{print $1}' /etc/shadow
+    echo "存在sudo权限的账号"
+    cat /etc/sudoers | grep -v "^#\|^$" | grep "ALL=(ALL)\|ALL=(ALL:ALL)"
+    echo "==============历史命令================"
+    user_list=$(ls /home)
+
+    for user in $user_list
+    do
+        echo "$user用户可疑命令:"
+        grep -e "bash" -e "wget" -e "ssh" /home/$user/.bash_history
+    done
+
+
+    echo "============异常端口============="
+    echo "waitting for develop"
+
+    echo "============异常进程============="
+    echo "waitting for develop"
+
+    echo "===========开机启动配置文件======="
+    cat /etc/rc.local
+    cat /etc/rc.d/rc[0~6].d
+
+    echo "===========计划任务============="
+    for user in $user_list
+    do
+        echo "$user用户计划任务:"
+        crontab -u $user -l
+    done
+    echo "==========可疑计划任务文件======="
+    a="/var/spool/cron/* 
+    /etc/crontab
+    /etc/cron.d/*
+    /etc/cron.daily/* 
+    /etc/cron.hourly/* 
+    /etc/cron.monthly/*
+    /etc/cron.weekly/
+    /etc/anacrontab
+    /var/spool/anacron/*"
+    for dir in $a
+    do
+        ls $dir 2>/dev/null
+    done
+    
+
+    
+}
+
+Log_Analysis(){
+    Log_Base="/var/log"
+    #得到一个日志文件列表
+    Log_list=$(ls $Log_Base |awk -F. '{print $1}' |uniq)
+    for Log_type in $Log_list
+    do 
+    eval "Log_Of_$Log_type" 2>/dev/null
+    done
+
+}
+function Log_Of_auth(){
+    pattern=${1:-"/var/log/auth.*"}
+    files=$(ls -t $pattern)
+    for file in $files
+    do 
+        if [ "${file##*.}" = "gz" ]; then
+            :
+        else
+            echo "$file文件分析:"
+            #ssh爆破失败
+            failure=$(grep --text "authentication failure" $file)
+            users=$(echo "$failure" |grep -oP 'user=\K\S+' |sort|uniq)
+            ips=$(echo "$failure" |grep -oP 'rhost=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+            echo "用户名    失败次数:"
+            for user in $users
+            do 
+                u=$(echo "$failure" |grep "user=$user")
+                i=$(echo "$u" |wc -l)
+                echo -n $user "     "
+                echo  $i
+                echo "爆破的IP"
+                echo -n  "$u" | grep -oP 'rhost=\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | uniq |awk -F '=' '{print $2}'
+            
+                echo '----------------------'
+            done
+
+            #ssh成功登录
+            echo "登录成功记录"
+            success=$(grep --text "Accepted password " $file)
+            # echo "$success"
+            time_user_ip=$(echo "$success" |awk '{print $1,$2,$3,$9,$11}')
+            echo "$time_user_ip"
+            echo "----------------------"
+
+            echo "爆破用户名字典"
+            grep -a "Failed password" $file| perl -e 'while($_=<>){ /for(.*?) from/; print "$1\n";}'|sort| uniq -c
+
+            echo "sudo命令执行历史"
+            sudo_history=$(grep "sudo:" $file |grep "COMMAND")
+            echo "$sudo_history" |head
+            
+        fi
+    done
+}
+function Log_Of_apache2(){
+    #access.log文件分析
+    pattern=${1:-"/var/log/apache2/access*"}
+    files=$(ls -t $pattern)
+    for file in $files
+    do 
+        if [ "${file##*.}" = "gz" ]; then
+            :
+        else
+            echo "$file文件分析:"
+            ips=$(cat $file |awk  '{print $1}' |uniq)
+            for ip in $ips
+                do
+                echo "$ip行为分析:" 
+                echo "状态码 次数"
+                cat "$file" |grep ^$ip |awk '{print $9}' |sort |uniq -c |awk '{print $2 "    " $1}'
+
+
+                done
+        fi
+    done
+
+    #error.log分析
+
+}
+
+
+
+function webshell(){
+    echo "===========webshell============"
+    rules=("eval(" "system(" "3c6e0b8a9c15224a")
+    result=""
+    length=${#rules[@]}
+    # echo $length
+    for ((i = 0; i<length;i++));do
+        if ((i==length-1));then
+            result="$result${rules[i]}"
+        else 
+            result="$result${rules[i]}\|"
+        fi
+    done
+    grep -r $result /var/www/html
+}
+
+main(){
+#    Basic_Info
+#    Invade_Identify
+#    Log_Analysis
+#    webshell
+}
+
+main
+exit
+
